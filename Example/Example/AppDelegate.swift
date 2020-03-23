a40bed9141fb1f035095eea0b1a23382b9cd66e0
Koji Murata <koji.murata@dena.com>
2020-03-23 13:14:35 +0900

    ios11

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -11,6 +11,8 @@ import FocusableImageView
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FocusableImageViewConfiguration.default = .init(
             backgroundColor: .init(white: 0, alpha: 0.5),
@@ -30,12 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         )
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
 
